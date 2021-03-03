117505b5ffde8b11b8ee3aa046bad2b0847f1475
zachmatson <zacharywdmatson@gmail.com>
2021-03-03 10:32:27 -0500

    Properly link sleef to rustc

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 fn main() {
-    cmake::Config::new("sleef")
+    let dst = cmake::Config::new("sleef")
         .define("BUILD_TESTS", "FALSE")
         .define("BUILD_SHARED_LIBS", "FALSE")
         .define("BUILD_GNUABI_LIBS", "FALSE")
+        .profile("Release")
         .build();
+    
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=sleef");
 
     println!("cargo:rerun-if-changed=sleef");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
 use std::path::{Path, PathBuf};
 
-const BASE_DIR: &'static str = env!("OUT_DIR");
-
-pub fn get_lib_dir() -> PathBuf {
-    Path::new(BASE_DIR).join("lib*")
-}
-
 pub fn get_header_dir() -> PathBuf {
-    Path::new(BASE_DIR).join("include")
+    Path::new(env!("OUT_DIR")).join("include")
 }
