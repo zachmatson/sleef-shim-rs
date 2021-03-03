1fdf2846571edeb12b50c676dc999ca4ab15285a
zachmatson <zacharywdmatson@gmail.com>
2021-03-02 21:31:17 -0500

    Add cargo rerun-if to build script

--- build.rs
+++ build.rs
@@ -2,5 +2,8 @@ fn main() {
     cmake::Config::new("sleef")
         .define("BUILD_TESTS", "FALSE")
         .define("BUILD_SHARED_LIBS", "FALSE")
+        .define("BUILD_GNUABI_LIBS", "FALSE")
         .build();
+
+    println!("cargo:rerun-if-changed=sleef");
 }
