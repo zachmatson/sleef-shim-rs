dec62174fc934a2436a5feb1951df8541c54a5d6
zachmatson <zacharywdmatson@gmail.com>
2021-04-04 17:17:12 -0400

    Make build require a cargo feature

--- build.rs
+++ build.rs
@@ -1,11 +1,16 @@
-fn main() {
-    let dst = cmake::Config::new("sleef")
-        .define("BUILD_TESTS", "FALSE")
-        .define("BUILD_SHARED_LIBS", "FALSE")
-        .define("BUILD_GNUABI_LIBS", "FALSE")
-        .define("CMAKE_INSTALL_LIBDIR", "lib")
-        .profile("Release")
-        .build();
+use std::env;
 
-    println!("cargo:rerun-if-changed=sleef");
+fn main() {
+    let build_sleef = env::var("CARGO_FEATURE_BUILD_SLEEF").is_ok();
+    if build_sleef {
+        cmake::Config::new("sleef")
+            .define("BUILD_TESTS", "FALSE")
+            .define("BUILD_SHARED_LIBS", "FALSE")
+            .define("BUILD_GNUABI_LIBS", "FALSE")
+            .define("CMAKE_INSTALL_LIBDIR", "lib")
+            .profile("Release")
+            .build();
+
+        println!("cargo:rerun-if-changed=sleef");
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,24 @@ use std::path::{Path, PathBuf};
 
 const BASE_DIR: &'static str = env!("OUT_DIR");
 
+fn warn_nobuild() -> ! {
+    panic!(
+        "sleef-shim was compiled without the \"build-sleef\" option. To use the SLEEF library, compile with this option."
+    )
+}
+
 pub fn get_lib_dir() -> PathBuf {
-    Path::new(BASE_DIR).join("lib")
+    if cfg!(feature = "build-sleef") {
+        Path::new(BASE_DIR).join("lib")
+    } else {
+        warn_nobuild()
+    }
 }
 
 pub fn get_header_dir() -> PathBuf {
-    Path::new(BASE_DIR).join("include")
+    if cfg!(feature = "build-sleef") {
+        Path::new(BASE_DIR).join("include")
+    } else {
+        warn_nobuild()
+    }
 }
